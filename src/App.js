95adaa29a65a72acfd299608518697c0874beac6
Mathew Torres <torres.mathew@gmail.com>
2022-09-26 14:40:37 -0700

    update repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      location: {},
+      error: false,
+      errorMessage: "",
+    };
+  }
+
+  handleInput= (event) => {
+    event.preventDefault();
+    this.setState({
+      searchQuery: event.target.value,
+    });
+  };
+
+  handleSearch = event => {
+    try {
+      event.preventDefault();
+      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      const res = await axios.get(API);
+      this.setState({ location: res.data[0] })
+    } catch (error) {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>App Component</h1>
+        <input type="text" onChange={event => this.setState({ searchQuery: event.target.value })} />
+        <button onClick={this.handleSearch}>Search</button>
+        <ul>
+          {this.state.searchResults.map(repo => (
+            <li key={repo.id}>{repo.name}</li>
+          ))}
+        </ul>
+        {this.state.error && <p>{this.state.error}</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
+
